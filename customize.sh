c36983c871013bbe18949d2979c5bf6be4616817
fxhack <fxhack@qq.com>
2021-01-02 19:37:08 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
-#=================================================
-#1. Modify default IP
+#修改默认管理IP，10.0.0.253改成你需要的IP
 sed -i 's/192.168.1.1/10.0.0.253/g' openwrt/package/base-files/files/bin/config_generate
