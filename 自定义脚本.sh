4446174a5cabfd48ef2729833c7e74517f9f9780
fxhack <fxhack@qq.com>
2021-01-02 22:53:15 +0800

    Update 自定义脚本.sh

--- 自定义脚本.sh
+++ 自定义脚本.sh
@@ -1,4 +1,5 @@
 #将config_generate文件内的192.168.1.1替换为10.0.0.253
 sed -i 's/192.168.1.1/10.0.0.253/g' openwrt/package/base-files/files/bin/config_generate
 #添加京东签到插件
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./openwrt/package/lean
+cd ./openwrt/package/lean
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
