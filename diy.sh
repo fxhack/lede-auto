80dbc52ed0774910d571226a89f3546e8101fbfe
fxhack <fxhack@qq.com>
2021-01-04 08:22:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,6 +1,6 @@
 sed -i '1i CONFIG_KERNEL_BUILD_USER="fxhack"' openwrt/.config
 sed -i '1i CONFIG_GRUB_TITLE="OpenWrt by fxhack"' openwrt/.config
-sed -i 's/192.168.1.1/10.0.0.253/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.5/g' openwrt/package/base-files/files/bin/config_generate
 #添加京东签到和应用过滤插件
 sed -i '3i CONFIG_PACKAGE_luci-app-jd-dailybonus=y' openwrt/.config
 sed -i '3i CONFIG_PACKAGE_luci-app-oaf=y' openwrt/.config
