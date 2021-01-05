9df0abc651b6a21079ca105cae0f2253a9f5e788
fxhack <fxhack@qq.com>
2021-01-05 11:21:59 +0800

    Update diy_currency.sh

--- diy_currency.sh
+++ diy_currency.sh
@@ -1,6 +1,6 @@
 sed -i '1i CONFIG_KERNEL_BUILD_USER="fxhack"' openwrt/.config
 sed -i '1i CONFIG_GRUB_TITLE="OpenWrt by fxhack"' openwrt/.config
-sed -i 's/192.168.1.1/10.0.0.5/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 #add applications
 #sed -i '3i CONFIG_PACKAGE_luci-app-jd-dailybonus=y' openwrt/.config
 #sed -i '3i CONFIG_PACKAGE_luci-app-oaf=y' openwrt/.config
