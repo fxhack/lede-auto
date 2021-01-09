ba99e0f959137feacd28b3b7c5af5ed1552f3912
fxhack <fxhack@qq.com>
2021-01-09 22:15:53 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,7 +5,7 @@ sed -i 's/192.168.1.1/10.0.0.5/g' openwrt/package/base-files/files/bin/config_ge
 #sed -i '3i CONFIG_PACKAGE_luci-app-jd-dailybonus=y' openwrt/.config
 #sed -i '3i CONFIG_PACKAGE_luci-app-oaf=y' openwrt/.config
 #sed -i '3i CONFIG_PACKAGE_luci-app-smartdns=y' openwrt/.config
-cd ./openwrt/package/lean
+#cd ./openwrt/package/lean
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
 #git clone https://github.com/destan19/OpenAppFilter.git
-git clone https://github.com/fxhack/packages_openwrt.git
+#git clone https://github.com/fxhack/packages_openwrt.git
