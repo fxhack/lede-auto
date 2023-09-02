fb8fc77f74857208acffe4a8a936e89656c790fb
fxhack <fxhack@qq.com>
2023-09-02 15:31:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,4 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
 #git clone https://github.com/destan19/OpenAppFilter.git
 #git clone https://github.com/fxhack/packages_openwrt.git
+git clone https://github.com/obanat/openwrt-printing-packages
