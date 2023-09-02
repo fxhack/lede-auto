8463c95cbe2b30337dbb9bbe11e43d240b267749
fxhack <fxhack@qq.com>
2023-09-02 15:59:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,11 +1,3 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i '1i src-git printing git://github.com/obanat/openwrt-printing-packages.git' ./feeds.conf.default
-
-#add applications
-#sed -i '3i CONFIG_PACKAGE_luci-app-jd-dailybonus=y' openwrt/.config
-#sed -i '3i CONFIG_PACKAGE_luci-app-oaf=y' openwrt/.config
-#sed -i '3i CONFIG_PACKAGE_luci-app-smartdns=y' openwrt/.config
-#cd ./openwrt/package/lean
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
-#git clone https://github.com/destan19/OpenAppFilter.git
-#git clone https://github.com/fxhack/packages_openwrt.git
+sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
