ac9b1802d86aa21445298068bb9bb888c5864274
fxhack <fxhack@qq.com>
2023-09-02 16:25:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,5 @@
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i '1i src-git printing https://github.com/obanat/openwrt-printing-packages.git' ./feeds.conf.default
 sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
-sed -i '1i CONFIG_KERNEL_BUILD_USER="fxhack"' openwrt/.config
-sed -i '1i CONFIG_GRUB_TITLE="OpenWrt by fxhack"' openwrt/.config
+sed -i 's/192.168.1.1/192.168.5.1/g' lede/package/base-files/files/bin/config_generate
+sed -i '1i CONFIG_KERNEL_BUILD_USER="fxhack"' lede/.config
+sed -i '1i CONFIG_GRUB_TITLE="OpenWrt by fxhack"' lede/.config
